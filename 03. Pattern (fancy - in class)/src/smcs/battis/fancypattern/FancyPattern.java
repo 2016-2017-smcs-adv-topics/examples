16430aa001033097e018cb43e7ba10f61bd0b20a
Seth Battis <sethbattis@stmarksschool.org>
2016-09-20 10:25:22 -0400

    The pattern moves!

--- 03. Pattern (fancy - in class)/src/smcs/battis/fancypattern/FancyPattern.java
+++ 03. Pattern (fancy - in class)/src/smcs/battis/fancypattern/FancyPattern.java
@@ -6,7 +6,7 @@ public class FancyPattern extends WindowController {
 	private Pattern p;
 	
 	public void begin() {
-		p = new Pattern(0, 0, canvas);
+		p = new Pattern(100, 100, canvas);
 	}
 	
 	public void onMouseClick(Location m) {
--- 03. Pattern (fancy - in class)/src/smcs/battis/fancypattern/Pattern.java
+++ 03. Pattern (fancy - in class)/src/smcs/battis/fancypattern/Pattern.java
@@ -16,23 +16,27 @@ public class Pattern {
 			if (row % 2 == 0) {
 				p[row] = new FilledOval[9];
 				for (int col = 0; col < 9; col++) {
-					p[row][col] = new FilledOval(20 * col + 10, 20 * row, 15, 15, canvas);
+					p[row][col] = new FilledOval(x + 20 * col + 10, y + 20 * row, 15, 15, canvas);
 				}
 			} else {
 				p[row] = new FilledOval[10];
 				for (int col = 0; col < 10; col++) {
-					p[row][col] = new FilledOval(20 * col, 20 * row, 15, 15, canvas);
+					p[row][col] = new FilledOval(x + 20 * col, y + 20 * row, 15, 15, canvas);
 				}
 			}
 		}
 	}
 
 	public void moveTo(Location m) {
+		double dx, dy;
+		dx = m.getX() - origin.getX();
+		dy = m.getY() - origin.getY();
 		for (int row = 0; row < p.length; row++) {
 			for (int col = 0; col < p[row].length; col++) {
-				/* move that oval */
+				p[row][col].move(dx, dy);
 			}
 		}
+		origin = m;
 	}
 
 }
