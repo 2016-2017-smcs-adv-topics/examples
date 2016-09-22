8fc57595c8985cc9f04ef74b166330e99b31fe57
sethbattis <sethbattis@10.1.105.176>
2016-09-22 14:48:44 -0400

    Moving the cursor

--- 04. Keyboard Events (in class)/src/smcs/battis/keyboardevents/KeyboardEventsThingie.java
+++ 04. Keyboard Events (in class)/src/smcs/battis/keyboardevents/KeyboardEventsThingie.java
@@ -1,13 +1,20 @@
 package smcs.battis.keyboardevents;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.*;
+import java.awt.event.*;
 
 import objectdraw.*;
 
 public class KeyboardEventsThingie extends WindowController implements KeyListener {
-	
+
+	private FilledOval cursor;
+
+	public final int INCREMENT = 1; // final means constant (no changing this
+									// value)
+
 	public void begin() {
+		cursor = new FilledOval(100, 100, 10, 10, canvas);
+		cursor.setColor(Color.RED);
 		canvas.addKeyListener(this);
 		canvas.requestFocus();
 	}
@@ -19,14 +26,31 @@ public class KeyboardEventsThingie extends WindowController implements KeyListen
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_UP:
+			cursor.move(0, -INCREMENT);
+			break;
+		case KeyEvent.VK_DOWN:
+			cursor.move(0, INCREMENT);
+			break;
+		case KeyEvent.VK_LEFT:
+			cursor.move(-INCREMENT, 0);
+			break;
+		case KeyEvent.VK_RIGHT:
+			cursor.move(INCREMENT, 0);
+			break;
+		case KeyEvent.VK_G:
+			cursor.setColor(Color.GREEN);
+			break;
+		case KeyEvent.VK_R:
+		default:
+			cursor.setColor(Color.RED);
+			break;
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
