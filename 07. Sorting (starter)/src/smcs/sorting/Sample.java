1c8f18ca7cb88d100e7dd4c8f2751716d3ae262f
Seth Battis <sethbattis@Pathfinder.local>
2016-10-20 19:01:28 -0400

    Set/Get examples

--- 07. Sorting (starter)/src/smcs/sorting/Sample.java
+++ 07. Sorting (starter)/src/smcs/sorting/Sample.java
@@ -9,20 +9,29 @@ public class Sample {
 		System.out.println(v); // ... and display it
 		// [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 		
+		/* get the element at index 3 */
+		System.out.println(v.get(3)); // ...and display it
+		// 3
+		
+		/* set the element at index 7 */
+		v.set(7, -2);
+		System.out.println(v); // ...and display it
+		// [0, 1, 2, 3, 4, 5, 6, -2, 8, 9]
+		
 		/* swap elements 2 and 6 */
 		v.swap(2, 6);
 		System.out.println(v); // ...and display it
-		// [0, 1, 6, 3, 4, 5, 2, 7, 8, 9]
+		// [0, 1, 6, 3, 4, 5, 2, -2, 8, 9]
 		
 		/* shuffle the elements once */
 		v.shuffle();
 		System.out.println(v); // ...and display it
-		// [3, 1, 0, 6, 4, 2, 9, 5, 7, 8]
+		// [4, 3, 0, -2, 9, 6, 1, 5, 8, 2]
 		
 		/* shuffle the elements 100 times */
 		v.shuffle(100);
 		System.out.println(v); // ...and display it
-		// [2, 7, 1, 9, 0, 6, 8, 5, 4, 3]
+		// [2, -2, 0, 1, 9, 6, 3, 4, 8, 5]
 
 	}
 	
