bdabfb2d73487631a4457b80af7e6e067d882dc9
amandachristy <test@youremail.com>
2016-11-08 10:37:07 -0500

    Update split()

--- 09. Mergesort in Place/src/smcs/mergesort/RandomList.java
+++ 09. Mergesort in Place/src/smcs/mergesort/RandomList.java
@@ -1,13 +1,12 @@
 package smcs.mergesort;
 
 public class RandomList {
-	
+
 	/*
-	 * Group 1: Gabe, Joey, Reid
-	 * Group 2: Janelle, Steven, Carol
-	 * Group 3: Amanda, Gillian, Sabin
+	 * Group 1: Gabe, Joey, Reid Group 2: Janelle, Steven, Carol Group 3:
+	 * Amanda, Gillian, Sabin
 	 */
-	
+
 	private ListNode head, tail;
 
 	/**
@@ -29,12 +28,10 @@ public class RandomList {
 	 */
 	public int size() {
 		int i = 0;
-		// TODO Group 2
-		for(ListNode j = head; j != null; j = head.getNext()){
-			 
+		for (ListNode j = head; j != null; j = head.getNext()) {
+
 			++i;
-			
-			
+
 		}
 		return i;
 	}
@@ -50,24 +47,24 @@ public class RandomList {
 	 * @postcondition List now ends at startOfSecondHalf-1
 	 */
 	public RandomList split(int startOfSecondHalf) {
-		// TODO Group 3
-		if(!isEmpty()){
-			
-		RandomList second = new RandomList();
-		ListNode count = head;
-		for(int i=0; i<startOfSecondHalf-1; i++){
-			count = count.getNext(); //count counts to the tail of first half
+		RandomList second = new RandomList(this);
+		ListNode finger = head;
+		for (int i = 0; i < (startOfSecondHalf - 1) && finger == null; i++) {
+			finger = finger.getNext(); // finger traverses to the tail of first
+										// half
 		}
-		second.add(count.getNext()); //head of second is the node after first half tail
-		second.add(tail); //add tail of current list
-		
-		this.tail = count; //change current list to only the first half of the list
-		return second; 
-		
-		}else{
-			return this;
-			System.out.println("empty list");
+		second.head = finger.getNext();
+		if (second.head == null) {
+			second.tail = null;
+		} else {
+			second.tail = tail;
 		}
+
+		this.tail = finger; // change current list to only the first half of
+							// the list
+		this.tail.setNext(null);
+
+		return second;
 	}
 
 	/**
@@ -76,8 +73,8 @@ public class RandomList {
 	 * @return whether or not the list is empty
 	 */
 	public boolean isEmpty() {
-		
-		if (head == null && tail == null){
+
+		if (head == null && tail == null) {
 			return true;
 		} else {
 			return false;
@@ -92,15 +89,14 @@ public class RandomList {
 	 */
 	public int head() {
 		// TODO Group 2
-		//return 0;
+		// return 0;
 		int a = 0;
-		if (head != null){
-		
-			 a =  head.getValue();
-			
-			//how do you get a avlue from a node
-				
-			
+		if (head != null) {
+
+			a = head.getValue();
+
+			// how do you get a avlue from a node
+
 		}
 		return a;
 	}
@@ -114,26 +110,23 @@ public class RandomList {
 	 */
 	public int pop() {
 		// TODO Group 2
-		//return 0;
+		// return 0;
 		int i = 0;
-		if(head != null){
-		 
-		 System.out.print(head.getValue());
-		  i = head.getValue();
-		 head = head.getNext();
-		
-		 
-		
-		//check head method
-		
-		
+		if (head != null) {
+
+			System.out.print(head.getValue());
+			i = head.getValue();
+			head = head.getNext();
+
+			// check head method
+
 		}
-		 return i;
+		return i;
 	}
 
 	private static void set(int x, int i) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	/**
